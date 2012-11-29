1e957683a3ed3e84fe8d1b9dacbbe345bac472d5
Orwell1984 <david.danssaert@gmail.com>
2012-11-29 01:31:43 +0100

    Made the temporary sensor block render 3D in inventory.

--- src/openccsensors/client/sensorperipheral/BlockSensorRenderingHandler.java
+++ src/openccsensors/client/sensorperipheral/BlockSensorRenderingHandler.java
@@ -1,11 +1,14 @@
 package openccsensors.client.sensorperipheral;
 
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 
 import openccsensors.OpenCCSensors;
+import openccsensors.common.sensorperipheral.TileEntitySensor;
 import net.minecraft.src.Block;
 import net.minecraft.src.IBlockAccess;
 import net.minecraft.src.RenderBlocks;
+import net.minecraft.src.TileEntityRenderer;
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 
 public class BlockSensorRenderingHandler implements ISimpleBlockRenderingHandler {
@@ -13,25 +16,31 @@ public class BlockSensorRenderingHandler implements ISimpleBlockRenderingHandler
 	@Override
 	public void renderInventoryBlock(Block block, int metadata, int modelID,
 			RenderBlocks renderer) {
-		GL11.glPushMatrix();
-		
+		// rotate the model 90 degrees
+		GL11.glRotatef(-90.0F, 0.0F, 1.0F, 0.0F);
+		// move it to the center
+        GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
+        // if it's a sensor block, render it like the tile entity
+		if (block == OpenCCSensors.Blocks.sensorBlock)
+        {
+        	TileEntityRenderer.instance.renderTileEntityAt(new TileEntitySensor(), 0.0D, 0.0D, 0.0D, 0.0F);
+        	GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+        }
 	}
 
 	@Override
 	public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z,
 			Block block, int modelId, RenderBlocks renderer) {
-		// renderer.renderStandardBlock(OpenCCSensors.Blocks.sensorBlock, x, y, z);
 		return false;
 	}
 
 	@Override
 	public boolean shouldRender3DInInventory() {
-		return false;
+		return true;
 	}
 
 	@Override
 	public int getRenderId() {
 		return OpenCCSensors.Config.sensorBlockRenderID;
 	}
-
 }
