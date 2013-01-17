41e7ab2bf15c7d08459aa49d13ad4930f2d13705
Mikee Franklin <mikeefranklin@gmail.com>
2013-01-17 21:07:24 +0000

    generic items! implemented all the ranges of cards. need to add recipes now, and have the sensors pay attention to upgrades

--- src/openccsensors/common/items/ItemGeneric.java
+++ src/openccsensors/common/items/ItemGeneric.java
@@ -15,9 +15,24 @@ import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
-public class ItemSensorUpgrade extends Item {
+public class ItemGeneric extends Item {
 
-	public ItemSensorUpgrade(int par1) {
+	
+	private static HashMap<Integer, ItemMetaData> metadata = new HashMap<Integer, ItemMetaData>();
+	
+	public static void register(ItemMetaData md) {
+		metadata.put(md.getId(), md);
+	}
+
+	public static ItemMetaData getMetaDataForItemStack(ItemStack stack) {
+		return getMetaDataForDamageValue(stack.getItemDamage());
+	}
+	
+	public static ItemMetaData getMetaDataForDamageValue(int damageValue) {
+		return metadata.get(damageValue);
+	}	
+
+	public ItemGeneric(int par1) {
 		super(par1);
 		setTextureFile("/openccsensors/resources/images/terrain.png");
 		setHasSubtypes(true);
@@ -29,19 +44,18 @@ public class ItemSensorUpgrade extends Item {
 	public int getIconFromDamage(int par1) {
 		return par1;
 	}
-
 	
 	@Override
 	public String getItemNameIS(ItemStack itemstack) {
-		return "";
+		return getMetaDataForItemStack(itemstack).getName();
 	}
 	
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void getSubItems(int par1, CreativeTabs tab, List subItems) {
-	//	for (Entry<Integer, SensorUpgradeTier> entry : upgrades.entrySet()) {
-	//		subItems.add(new ItemStack(par1, 1, entry.getValue().getId()));
-	//	}
+		for (Entry<Integer, ItemMetaData> entry : metadata.entrySet()) {
+			subItems.add(new ItemStack(par1, 1, entry.getValue().getId()));
+		}
 	}
 	
 }
