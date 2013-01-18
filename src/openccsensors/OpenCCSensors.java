1d46add1c849e3f75a66b8061e6acab9695a85fe
Mikee Franklin <mikeefranklin@gmail.com>
2013-01-18 22:14:52 +0000

    updated version number

--- src/openccsensors/OpenCCSensors.java
+++ src/openccsensors/OpenCCSensors.java
@@ -16,7 +16,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
-@Mod( modid = "OCS", name = "OpenCCSensors", version = "0.1.1", dependencies = "required-after:ComputerCraft;after:CCTurtle;after:BuildCraft|Core;after:IC2;after:Thaumcraft")
+@Mod( modid = "OCS", name = "OpenCCSensors", version = "0.1.2", dependencies = "required-after:ComputerCraft;after:CCTurtle;after:BuildCraft|Core;after:IC2;after:Thaumcraft")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class OpenCCSensors 
 {
