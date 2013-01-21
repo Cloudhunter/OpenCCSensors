16b19835dae2f6cf8df710d456d9f47a6544b670
Mikee Franklin <mikeefranklin@gmail.com>
2013-01-21 17:25:24 +0000

    fleshed out the minecraft target a bit

--- src/openccsensors/common/helper/IC2Helper.java
+++ src/openccsensors/common/helper/IC2Helper.java
@@ -1,5 +1,8 @@
 package openccsensors.common.helper;
 
+import openccsensors.common.items.ItemSensorCard;
+import cpw.mods.fml.common.Loader;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 public class IC2Helper {
@@ -9,5 +12,14 @@ public class IC2Helper {
 			itemStack = ic2.api.Items.getItem(itemName);
 		}catch(Exception e) {}
 		return itemStack;
+	}
+
+	public static void addTier1CardRecipe() {
+		ItemStack icStack = IC2Helper.getItemStack("copperCableItem");
+		if (icStack == null) {
+			icStack = new ItemStack(Item.flint);
+		}
+		RecipeHelper.addTier1CardRecipe(ItemSensorCard.INDUSTRIALCRAFT_TIER_1, icStack);
+	
 	}
 }
