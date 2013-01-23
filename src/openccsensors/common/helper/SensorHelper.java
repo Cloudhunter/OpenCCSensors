1d87cea7d612e12c6e26e5c63945a35b29e4574a
Mikee Franklin <mikeefranklin@gmail.com>
2013-01-23 19:04:06 +0000

    trying to sort this item name shit!!!

--- src/openccsensors/common/helper/SensorHelper.java
+++ src/openccsensors/common/helper/SensorHelper.java
@@ -9,22 +9,29 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 public class SensorHelper {
 
 	public static String getType(String rawType) {
-		String translated = LanguageRegistry.instance().getStringLocalization(
-				rawType, "en_OCS");
+		String translated = LanguageRegistry.instance().getStringLocalization(rawType);
 		return translated == "" ? rawType : translated;
 	}
 	
 	public static String getRawType(TileEntity entity) {
-		String rawType = entity.getClass().getName();
-		Block b = entity.getBlockType();
-		if (b != null) {
-			rawType = b.getBlockName();
+		ItemStack stack = new ItemStack(entity.getBlockType(), 1, entity.getBlockMetadata());
+		
+		String rawName = entity.getClass().getName().toLowerCase();
+		try {
+			rawName = stack.getItemName().toLowerCase();
+		}catch(Exception e) {			
 		}
-		return rawType.toLowerCase().trim();
+		String packageName = entity.getClass().getName().toLowerCase();
+		String[] packageLevels = packageName.split("\\.");
+		if (!rawName.startsWith(packageLevels[0])) {
+			rawName = packageLevels[0] + "." + rawName;
+		}
+		
+		return rawName;
 	}
 	
 	public static String getDisplayType(TileEntity entity) {
-		ItemStack stack = new ItemStack(entity.getBlockType());
+		ItemStack stack = new ItemStack(entity.getBlockType(), 1, entity.getBlockMetadata());
 		return InventoryHelper.getNameForItemStack(stack);
 	}
 	
