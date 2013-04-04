9097744676e097a7c26bca43ed7ac8bee023456a
Azenet <shellgratuit@gmail.com>
2013-04-04 02:03:53 +0200

    Removed useless EntityUtils.getTileEntities.

--- src/openccsensors/common/util/EntityUtils.java
+++ src/openccsensors/common/util/EntityUtils.java
@@ -49,37 +49,8 @@ public class EntityUtils {
 
 		return map;
 	}
+
 	
-	public static HashMap<String, TileEntity> getTileEntities(World world, Vec3 sensorPos, double radius, Class filter) {
-		HashMap<String, TileEntity> map = new HashMap<String, TileEntity>();
-		
-		int dChunk = (int) Math.ceil(radius / 16.0F);
-		
-		int x = (int)sensorPos.xCoord;
-		int y = (int)sensorPos.yCoord;
-		int z = (int)sensorPos.zCoord;
-
-		for (int dx = -dChunk; dx <= dChunk; dx++) {
-			for (int dz = -dChunk; dz <= dChunk; dz++) {
-				Chunk chunk = world.getChunkFromBlockCoords(x+16*dx, z+16*dz);
-				TileEntity te = (TileEntity)chunk.chunkTileEntityMap.get(new ChunkPosition(x+16*dx, 0, z+16*dz));
-				Vec3 livingPos = Vec3.createVectorHelper(
-						te.xCoord + 0.5,
-						te.yCoord + 0.5,
-						te.zCoord + 0.5
-				);
-				if (sensorPos.distanceTo(livingPos) <= radius && filter.isAssignableFrom(te.getClass())) {
-					String targetName = te.getClass().getName();
-					targetName = targetName.replaceAll("\\s", "");
-					map.put(targetName, te);
-				}
-			}
-		}
-
-		return map;
-	}	
-	
-
 	public static HashMap livingToMap(EntityLiving living, boolean additional) {
 		HashMap map = new HashMap();
 		
