121ddf622e87d6eb4b7f7a4946e6fd1e64d5ced0
Christopher Beach <synthetic.actuality@gmail.com>
2014-01-11 21:41:41 -0800

    railcraft api/utils update

--- src/openccsensors/common/util/RailcraftUtils.java
+++ src/openccsensors/common/util/RailcraftUtils.java
@@ -1,44 +1,51 @@
 package openccsensors.common.util;
 
-import java.lang.reflect.Method;
+import ic2.api.tile.IEnergyStorage;
 
+import java.util.HashMap;
+
+import buildcraft.api.power.IPowerReceptor;
+import buildcraft.api.power.PowerHandler.PowerReceiver;
+import mods.railcraft.api.carts.IEnergyTransfer;
+import mods.railcraft.api.carts.IExplosiveCart;
+import net.minecraft.entity.item.EntityMinecart;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraftforge.liquids.ILiquidTank;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
+
 
 public class RailcraftUtils {
-
-	public static String ITANKTILE_NAME = "railcraft.common.blocks.machine.ITankTile";
-
-	public static Class tankTile = null;
 	
-	public static boolean isTankTile(TileEntity tile) {
-		return getTankIfTankTile(tile) != null;
-	}
-
-
-	public static ILiquidTank getTankIfTankTile(TileEntity entity)
-	{
-		if (tankTile == null) {
-			try {
-				tankTile = Class.forName(ITANKTILE_NAME);
-			} catch (ClassNotFoundException e) {
+	public static HashMap getEnergyDetails(EntityMinecart minecart) {
+		HashMap response = new HashMap();
+		if (minecart instanceof IEnergyTransfer) {
+			IEnergyTransfer energyCart = (IEnergyTransfer) minecart;
+			int capacity = energyCart.getCapacity();
+			int stored = energyCart.getEnergy();
+			
+			response.put("Stored", stored);
+			response.put("Capacity", capacity);
+			response.put("Output", energyCart.getTransferLimit());
+			response.put("StoredPercentage", 0);
+			
+			if (capacity > 0) {
+				response.put("StoredPercentage", Math.max(Math.min(100,((100.0 / capacity) * stored)), 0));
 			}
 		}
-
-		ILiquidTank tank = null;
-		if (tankTile != null) {
-			if (tankTile.isAssignableFrom(entity.getClass())) {
-				Method tankMethod = null;
-				try {
-					tankMethod = tankTile.getMethod("getTank");
-					if (tankMethod != null) {
-						tank = (ILiquidTank)tankMethod.invoke(entity);
-					}
-				} catch (Exception e) {
-				}
+		return response;
+	}
+	
+	public static HashMap getExplosiveDetails(EntityMinecart minecart) {
+		HashMap response = new HashMap();
+		if (minecart instanceof IExplosiveCart) {
+			IExplosiveCart explosiveCart = (IExplosiveCart) minecart;
+			response.put("IsPrimed", explosiveCart.isPrimed());
+			if (explosiveCart.isPrimed()) {
+				response.put("FuseLength", explosiveCart.getFuse());
+				response.put("BlastRadius", explosiveCart.getBlastRadius());
 			}
 		}
-
-		return tank;
+		return response;
 	}
+
 }
