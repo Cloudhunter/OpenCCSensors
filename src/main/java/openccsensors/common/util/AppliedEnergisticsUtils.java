b9a895ade55d801fabb9d62656a1d3378f05f0a3
Christopher Beach <synthetic.actuality@gmail.com>
2014-09-30 19:28:36 -0700

    update applied energistics api

--- src/main/java/openccsensors/common/util/AppliedEnergisticsUtils.java
+++ src/main/java/openccsensors/common/util/AppliedEnergisticsUtils.java
@@ -5,10 +5,17 @@ import java.util.List;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import appeng.api.me.tiles.IGridTileEntity;
-import appeng.api.me.util.IGridInterface;
-import appeng.api.me.util.IMEInventory;
-import appeng.api.me.util.IMEInventoryHandler;
+import net.minecraftforge.common.util.ForgeDirection;
+import appeng.api.AEApi;
+import appeng.api.networking.IGridBlock;
+import appeng.api.networking.IGridNode;
+import appeng.api.networking.energy.IEnergyGrid;
+import appeng.api.storage.ICellInventory;
+import appeng.api.storage.IMEInventory;
+import appeng.api.storage.IMEInventoryHandler;
+import appeng.api.storage.data.IAEItemStack;
+import appeng.api.storage.data.IAEStack;
+import appeng.api.storage.data.IItemList;
 
 public class AppliedEnergisticsUtils {
 
@@ -28,48 +35,58 @@ public class AppliedEnergisticsUtils {
 		TileEntity aeWirelessAPtileEntity = (TileEntity) obj;
 		response.put("Powered", false);
 
-		IGridTileEntity aeMachine = (IGridTileEntity) aeWirelessAPtileEntity;
-		IGridInterface gi = aeMachine.getGrid();
-		if (gi != null && gi.isValid() && aeMachine.isPowered()) {
+		IGridBlock aeMachine = (IGridBlock) aeWirelessAPtileEntity;
+		IGridNode gi = aeMachine.getMachine().getGridNode(ForgeDirection.UNKNOWN);
+		if (aeMachine instanceof IEnergyGrid && ((IEnergyGrid)aeMachine).isNetworkPowered() && aeMachine instanceof IMEInventoryHandler) {
 
-			IMEInventoryHandler imivh = gi.getFullCellArray();
+			IMEInventoryHandler imivh = (IMEInventoryHandler) aeMachine;
 			IMEInventory imiv = (IMEInventory) imivh;
+			IEnergyGrid eGrid = (IEnergyGrid) aeMachine;
 			response.put("Powered",true);
 			
-			//user IMEInventory for remaining item types and count.
-			response.put("FreeTypes", (int) imiv.remainingItemTypes());
-			response.put("FreeCount", (int) imiv.remainingItemCount());
-			response.put("FreeBytes", (int) imivh.freeBytes());
-			response.put("UsedBytes", (int) imivh.usedBytes());
-			response.put("TotalBytes", (int) imivh.totalBytes());
+			if (imiv instanceof ICellInventory) {
+				//uses ICellInventory for remaining item types and count.
+				ICellInventory cells = (ICellInventory) imiv;
+				response.put("FreeTypes", (int) cells.getRemainingItemTypes());
+				response.put("FreeCount", (int) cells.getRemainingItemCount());
+				response.put("FreeBytes", (int) cells.getFreeBytes());
+				long usedBytes = cells.getUsedBytes();
+				long totalBytes = cells.getTotalBytes();
+				response.put("UsedBytes", (int) usedBytes);
+				response.put("TotalBytes", (int) totalBytes);
 			
-			long totalBytes = imivh.totalBytes();
+				
 			
-			double percent = (double)100 / totalBytes * imivh.usedBytes();
-			percent = Math.max(Math.min(percent, 100), 0);
-			response.put("InventoryPercentFull", percent);
+				double percent = (double)100 / totalBytes * usedBytes;
+				percent = Math.max(Math.min(percent, 100), 0);
+				response.put("InventoryPercentFull", percent);
+				response.put("CanHoldNewItems", cells.canHoldNewItem());
+			}
 			
 			if (additional) {
-			
-				List<ItemStack> list = imiv.getAvailableItems().getItems();
+				IItemList list = AEApi.instance().storage().createItemList();
+				imiv.getAvailableItems(list);
 				int totalCount = 0;
+				int i = 0;
 				HashMap stacks = new HashMap();
-				for (int i = 0; i < list.size(); i++) {
-					ItemStack items = list.get(i);
-					stacks.put(i + 1, InventoryUtils.itemstackToMap(items));
-					Integer count = items.stackSize;
-					totalCount += count;				
-				}
+				Iterable<IAEStack> iterator = (Iterable<IAEStack>)list.iterator();
+				for (IAEStack stack : iterator) {
+					if (stack.isItem()) {
+						IAEItemStack itemStack = (IAEItemStack) stack;
+						ItemStack items = itemStack.getItemStack();
+						stacks.put(i++, InventoryUtils.itemstackToMap(items));
+						totalCount += items.stackSize;
+					}
+				};
 				
 				response.put("UsedTypes", list.size());
 				response.put("UsedCount", totalCount);
 				response.put("Slots", stacks);
 				
-				response.put("CanHoldNewItems", imivh.canHoldNewItem());
+				
 				response.put("Priority", imivh.getPriority());
-				response.put("SystemPower", gi.getPowerUsageAvg());
+				response.put("SystemPower", eGrid.getAvgPowerUsage());
 			}
-			gi.triggerPowerUpdate();
 
 		}
 		
